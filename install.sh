b74507b05b1bbde7a4f115ccb207ee761630a44d
Felix Krause <flyx@isobeef.org>
2014-01-22 19:13:29 +0100

    fix install path

--- install.sh
+++ install.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
 apt-get install puppet
-git clone https://github.com/shackspace/hexaeder.git /tmp
+git clone https://github.com/shackspace/hexaeder.git /tmp/hexaeder
 puppet apply /tmp/hexaeder/manifests/setup.pp
